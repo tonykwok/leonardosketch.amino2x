e643bdc499f7eaae22f640b8e32a8bcdfa3b9b90
Josh Marinacci <joshua@marinacci.org>
2011-10-29 21:37:52 -0700

    fixed sphere lighting

---
+++ src/java/jogl/src/com/joshondesign/amino/jogl/JoglLayer.java
@@ -0,0 +1,12 @@
+package com.joshondesign.amino.jogl;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: josh
+ * Date: 10/29/11
+ * Time: 9:32 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public interface JoglLayer {
+    public void setSceneSize(int width, int height);
+}
